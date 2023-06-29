be793f771cdc4105068a241bcf4c9248bab865eb
cristian maizon <ismael.maizon1234@gmail.com>
2023-06-29 00:14:17 -0300

    segundo commit

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express  from "express";
+import handlebars from "express-handlebars";
+import __dirnmae from "./utils.js";
 import routerProducts from './router/products.js';
 import routerCarts from './router/carts.js';
 
@@ -6,7 +8,13 @@ import routerCarts from './router/carts.js';
 const app = express();
 
 
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirnmae + '/views');
+app.set('view engine','handlebars');
+
+
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 
 app.use('/api/products', routerProducts)
